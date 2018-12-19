862ef4e9f67b14fbe8148f9032d6bdf4492c561d
Diego <drh217@alumnos.unican.es>
2018-12-19 12:36:45 +0100

    Reserva y TipoHabitacion creadas

--- GestionHoteles/GestionHotelesCommon/src/main/java/is/unican/es/dominio/Reserva.java
+++ GestionHoteles/GestionHotelesCommon/src/main/java/is/unican/es/dominio/Reserva.java
@@ -1,5 +1,55 @@
 package is.unican.es.dominio;
 
+import java.util.Date;
+
 public class Reserva {
 
+	private int id;
+	private Date fechaEntrada;
+	private Date fechaSalida;
+	private double precio;
+	
+	public Reserva() {
+		
+	}
+	
+	public Reserva(int id, Date fechaEntrada, Date fechaSalida, double precio) {
+		super();
+		this.id = id;
+		this.fechaEntrada = fechaEntrada;
+		this.fechaSalida = fechaSalida;
+		this.precio = precio;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Date getFechaEntrada() {
+		return fechaEntrada;
+	}
+
+	public void setFechaEntrada(Date fechaEntrada) {
+		this.fechaEntrada = fechaEntrada;
+	}
+
+	public Date getFechaSalida() {
+		return fechaSalida;
+	}
+
+	public void setFechaSalida(Date fechaSalida) {
+		this.fechaSalida = fechaSalida;
+	}
+
+	public double getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
 }
--- GestionHoteles/GestionHotelesCommon/src/main/java/is/unican/es/dominio/TipoHabitacion.java
+++ GestionHoteles/GestionHotelesCommon/src/main/java/is/unican/es/dominio/TipoHabitacion.java
@@ -2,4 +2,43 @@ package is.unican.es.dominio;
 
 public class TipoHabitacion {
 
+	private String descripcion;
+	private double precioPorNoche;
+	private int disponibles;
+	
+	public TipoHabitacion() {
+		
+	}
+
+	public TipoHabitacion(String descripcion, double precioPorNoche, int disponibles) {
+		super();
+		this.descripcion = descripcion;
+		this.precioPorNoche = precioPorNoche;
+		this.disponibles = disponibles;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public double getPrecioPorNoche() {
+		return precioPorNoche;
+	}
+
+	public void setPrecioPorNoche(double precioPorNoche) {
+		this.precioPorNoche = precioPorNoche;
+	}
+
+	public int getDisponibles() {
+		return disponibles;
+	}
+
+	public void setDisponibles(int disponibles) {
+		this.disponibles = disponibles;
+	}
+	
 }
